b9def6a0d9665abcf4d9c4df1d3b945092f77e56
Oscar Idiaquez <oscaridiaquezmiller@gmail.com>
2020-10-02 09:34:15 -0500

    Nuevo Archivo

---
+++ app.js
@@ -0,0 +1,15 @@
+const sumar = (y, x) => {
+  return x + y;
+};
+
+const restar = (x, y) => {
+  return x - y;
+};
+
+const multiplicar = (y, x) => {
+  return x * y;
+};
+
+const dividir = (x, y) => {
+  return x / y;
+};
